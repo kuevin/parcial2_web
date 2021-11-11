ce99259915303478d8736df91e5f255f5ef6b919
kuevin <94088635+kuevin@users.noreply.github.com>
2021-11-10 18:06:35 -0700

    detalle de la practica 1 PHP, impresion de nums

---
+++ practica1/practica1.php
@@ -0,0 +1,30 @@
+<?php 
+
+  echo "Hola mundo";
+  echo "<br> <br>";
+
+  echo "<h1 style='color:red;'>Lista de numeros</h1>";
+
+  for ($i=0; $i < 10 ; $i++) { 
+    echo $i+1 . "<br>";
+  }
+
+  for ($i=0; $i < 10; $i++) { 
+    
+    echo "<div style='border:1px solid black; padding:30px; width:30px; display:inline-flex; background-color:green'>";
+    echo $i+1;
+    echo "</div>";
+  }
+  echo "<br>";
+  for ($i=0; $i < 10 ; $i++) { 
+    
+  if ($i % 2 == 0){
+      echo "<div style='border: 1px solid green; padding:30px; width:30px; display:inline-flex; background-color:blue'>";
+    } else {
+      echo "<div style='border: 1px solid green; padding:30px; width:30px; display:inline-flex; background-color:orange'>";
+    }
+
+    echo $i+1;
+    echo "</div>";
+  }
+ ?>
