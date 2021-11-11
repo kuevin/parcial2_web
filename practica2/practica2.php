792992a3e19f9c19cb2ec510693ad2635de8c387
kuevin <94088635+kuevin@users.noreply.github.com>
2021-11-10 18:16:08 -0700

    Add files via upload

---
+++ practica2/practica2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title> Practica 2</title>
+</head>
+<body>
+	<?php 
+    $numero= rand(1,4);
+    $color = ["#36f443", "#f44336", "#bdfcff", "#fff5bc"];
+    $numcolor= rand(0,3);
+    echo "Numero generado = ". $numero . "<br>";
+    echo "<div style='color:".$color[$numcolor]."'> color generado </div>";
+    if ($numero == 1) {
+    	
+    ?>
+    <h1 style="color: <?php  echo $color[$numcolor]; ?>">Titulo de la pagina 1</h1>
+    <?php  }  elseif ($numero == 2) {?>
+    	<h2 style="color: <?php  echo $color[$numcolor]; ?>">Titulo 2</h2>
+    <?php } elseif ($numero == 3) {?>
+     <h3 style="color: <?php  echo $color[$numcolor]; ?>"> Titulo 3 de la pagina</h3>
+    <?php } else {?>
+    	<h4 style="color: <?php  echo $color[$numcolor]; ?>">Titulo 4 de la pagina</h4>
+    <?php } ?>
+</body>
+</html>
